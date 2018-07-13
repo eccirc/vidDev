c2f5203c2542df9e86dd07df648966dea81eee83
DavidGW <dgw_88@outlook.com>
2018-07-13 20:21:30 +0100

    Add slit scanning vert & hoz

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,14 +3,17 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetBackgroundColor(255);
-    w = ofGetWidth()/2;
-    h = ofGetHeight()/2;
+//    w = ofGetWidth()/2;
+//    h = ofGetHeight()/2;
 
-    grainSize = 2;
+    w = 320;
+    h = 240;
+
+    grainSize = 1;
 
 
 
-    myCam.setDesiredFrameRate(30);
+    myCam.setDesiredFrameRate(5);
 
 
     myCam.initGrabber(w,h);
@@ -48,9 +51,10 @@ void ofApp::draw(){
 
     for(int x = 0; x < w; x += grainSize*2){
         for(int y = 0; y < h; y += grainSize*2){
+            ofPushStyle();
             ofColor colXY = ImgBuffer[0].getColor(x,y);
             int colBright = colXY.getBrightness();
-            float colMap = ofMap(colBright, 0, 255, 0, 5);
+            float colMap = ofMap(colBright, 0, 255, 5, 0);
             int b;
             //binarise the values to get just black or white
             if(colBright < 175){
@@ -63,6 +67,7 @@ void ofApp::draw(){
             ofSetCircleResolution(20);
             //ofSetColor(colBright);
             ofDrawCircle(x,y, grainSize);
+            ofPopStyle();
         }
     }
 
@@ -70,10 +75,15 @@ void ofApp::draw(){
 
     //Now to figure out how to get a slitscan from the image above...
         for(int i = 0; i < sGrab.size(); i ++){
-            ofSetColor(255, 0, 0);
-            ofSetColor(255,255,255);
-            sGrab[i].drawSubsection(i,h,grainSize,h,i + 2, 0);
+            ofPushStyle();
+            ofSetColor(255);
+            //vertical scan
+            sGrab[i].drawSubsection(i,h,grainSize,h,i, 0);
+            //horizontal scan
+            sGrab[i].drawSubsection(0, i + h + h,w,grainSize, 0, i);
+            ofPopStyle();
         }
+
 
 
 
